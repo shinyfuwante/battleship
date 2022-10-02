cb1c7f7f5535695c708290f9df12c0c950a7c1a1
shinyfuwante <mr.philliptang@gmail.com>
2022-10-02 01:51:00 -0700

    Implement receiveAttack and fix initialization of boards
    This commit implements the receiveAttack and checkGameOver methods. In addition, when
    testing receiveAttack, found that making an Array the way previously created a single array where
    all of the elements referenced the same array. This is due to array fill populating every element with
    the same reference to an array. To fix this, the lines were changed to create a new Array from an array
    of the desired size with the callback function to create a new array of the second dimension's size.

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,5 +1,9 @@
 const gameboard = (size = 7) => {
-    const board = new Array(size).fill(new Array(size));
+    const board = Array.from(new Array(size), x => new Array(size));
+    // track shots on board
+    const shots = Array.from(new Array(size), x => new Array(size));
+    // track number of ships on board
+    let ships = [];
 
     function isValidPlacement (ship, row, col, isVertical) {
         // check bounds
@@ -32,11 +36,34 @@ const gameboard = (size = 7) => {
                 board[row][col+i] = ship;
             }
         }
+        ships.push(ship);
         return true;
+    }
+
+    function receiveAttack(row, col) {
+        if (row < 0 || row > size - 1) return false;
+        if (col < 0 || col > size - 1) return false;
+        
+        if (shots[row][col] != null) return false;
+        if (board[row][col]) {
+            // process the ship that was hit
+            board[row][col].hit();
+            // update shots board
+            shots[row][col] = true;
+        } else {
+            shots[row][col] = false;
+        }
+        return true;
+    }
+    function checkGameOver() {
+        const isSunk = (ship) => ship.isSunk();
+        return ships.every(isSunk)
     }
     return {
         board,
-        placeShip
+        placeShip,
+        receiveAttack,
+        checkGameOver
     }
 }
 
