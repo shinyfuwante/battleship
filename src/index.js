b8981ab8c7ebb81ad5c078462dbd682ebd024d21
shinyfuwante <mr.philliptang@gmail.com>
2022-10-09 23:07:31 -0700

    Let only right side be clickable during game, move domAI to manipDom

--- src/index.js
+++ src/index.js
@@ -12,26 +12,16 @@ const rightGB = gameboard();
 const domRightGB = document.querySelector(".right-gameboard");
 const DM = manipulateDom(leftGB, rightGB);
 
-function domAI(player, board) {
-    const domBoard = board === leftGB ? domLeftGB : domRightGB;
-    const pixelList = Array.from(domBoard.childNodes);
-    // need to parseInt because dataset.row is a string. Add a radix of 10 for decimal numbers.
-    let attackSuccess = false;
-    while (attackSuccess === false) {
-        const [row, col] = player.randomMove();
-        const targetPixel = pixelList.filter((pixel) => parseInt(pixel.dataset.row, 10) === row && parseInt(pixel.dataset.col, 10) === col);
-        attackSuccess = DM.domReceiveAttack(board, targetPixel[0], row, col);
-    };
-}
-
 const testButton = document.querySelector('button');
-testButton.addEventListener('click', () => domAI(player2, leftGB))
+testButton.addEventListener('click', () => manipulateDom.domAI(player2, leftGB))
 
 DM.createBoard(domLeftGB, leftGB);
 DM.createBoard(domRightGB, rightGB);
-DM.addEventListeners();
+
 leftGB.populateShipsRandomly();
 rightGB.populateShipsRandomly();
+
+DM.addEventListeners();
 
 // implement basic "ai" (just random shots);
 
